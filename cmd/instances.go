a1e37cf6a25657be72c804a85952f1bad5b0b0d0
Lou Ferrand <ferrand@ekino.com>
2018-03-16 14:30:24 +0100

    Display container instance attributes

--- cmd/instances.go
+++ cmd/instances.go
@@ -3,23 +3,39 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/mitchellh/colorstring"
 	"github.com/spf13/cobra"
 )
 
-var instancesFilter string
+const timeFormat = "2018-02-23 10:46:01 +0000 UTC"
+
+var (
+	instancesFilter     string
+	instancesLongOutput bool
+)
 var instancesCmd = &cobra.Command{
 	Use:   "instances",
 	Short: "List container instances in your ECS clusters",
 	Run:   runCommandInstances,
 }
 
+type eInstance struct {
+	IPAddress string
+	ImageID   string
+	Name      string
+}
+
 func init() {
 	rootCmd.AddCommand(instancesCmd)
 
-	instancesCmd.Flags().StringVarP(&instancesFilter, "filter", "f", "", "Filter by the name of the ECS cluster")
+	instancesCmd.Flags().StringVarP(&instancesFilter, "cluster", "c", "", "Filter by the name of the ECS cluster")
+	instancesCmd.Flags().BoolVarP(&instancesLongOutput, "long", "l", false, "Enable detailed output of containers instances")
 }
 
 func runCommandInstances(cmd *cobra.Command, args []string) {
@@ -64,37 +80,79 @@ func runCommandInstances(cmd *cobra.Command, args []string) {
 			fmt.Println("Failed to describe EC2 instances: " + err.Error())
 			os.Exit(1)
 		}
-		instancesAttrs := make(map[string]map[string]string)
+		instances := make(map[string]eInstance)
 		for _, res := range describeInstanceResp.Reservations {
 			for _, inst := range res.Instances {
-				instance := map[string]string{
-					"IpAddress": *inst.PrivateIpAddress,
-					"ImageId":   *inst.ImageId,
-					"NameTag":   *findTag(inst.Tags, "Name").Value,
+				instances[*inst.InstanceId] = eInstance{
+					IPAddress: *inst.PrivateIpAddress,
+					ImageID:   *inst.ImageId,
+					Name:      *findTag(inst.Tags, "Name").Value,
 				}
-				instancesAttrs[*inst.InstanceId] = instance
 			}
 		}
 		fmt.Printf(
-			"%-20s  %-8s %5s  %8s %8s  %8s %8s  %15s %12s  %5v  %-12s  %s\n",
+			"%-20s  %-8s %5s  %8s %8s  %8s %8s  %15s %12s  %6v  %-12s  %-8s  %11s  %s\n",
 			"INSTANCE ID", "STATUS", "TASKS", "CPU/used", "CPU/free",
 			"MEM/used", "MEM/free", "PRIVATE IP", "INST.TYPE", "AGENT",
-			"IMAGE", "NAME",
+			"IMAGE", "DOCKER", "AGE", "NAME",
 		)
 		for _, cinst := range describeContainerInstancesResp.ContainerInstances {
+			agentVersion := colorstring.Color("[green]" + *cinst.VersionInfo.AgentVersion)
+			if *cinst.AgentConnected == false {
+				agentVersion = colorstring.Color("[red]" + *cinst.VersionInfo.AgentVersion)
+			}
 			registeredCPU := *findResource(cinst.RegisteredResources, "CPU").IntegerValue
 			remainingCPU := *findResource(cinst.RemainingResources, "CPU").IntegerValue
 			registeredMemory := *findResource(cinst.RegisteredResources, "MEMORY").IntegerValue
 			remainingMemory := *findResource(cinst.RemainingResources, "MEMORY").IntegerValue
 			instanceType := *findAttribute(cinst.Attributes, "ecs.instance-type").Value
-			instanceAttrs := instancesAttrs[*cinst.Ec2InstanceId]
+			dockerVersion := strings.TrimPrefix(*cinst.VersionInfo.DockerVersion, "DockerVersion: ")
+			ageInDays := fmt.Sprintf("%4.1f days", time.Since(*cinst.RegisteredAt).Hours()/24)
+			instance := instances[*cinst.Ec2InstanceId]
 			fmt.Printf(
-				"%-20s  %-8s %5d  %8d %8d  %8d %8d  %15s %12s  %-5v  %12s  %s\n",
+				"%-20s  %-8s %5d  %8d %8d  %8d %8d  %15s %12s  %-6v  %12s  %10s  %s  %s\n",
 				*cinst.Ec2InstanceId, *cinst.Status, *cinst.RunningTasksCount,
 				registeredCPU-remainingCPU, remainingCPU, registeredMemory-remainingMemory, remainingMemory,
-				instanceAttrs["IpAddress"], instanceType, *cinst.AgentConnected, instanceAttrs["ImageId"],
-				instanceAttrs["NameTag"],
+				instance.IPAddress, instanceType, agentVersion, instance.ImageID,
+				dockerVersion, ageInDays, instance.Name,
 			)
+			instanceAttributes := []string{}
+			capabilities := []string{}
+			var line string
+			if instancesLongOutput == true {
+				for _, attr := range cinst.Attributes {
+					if strings.Contains(*attr.Name, "ecs.capability.") {
+						capability := strings.SplitAfter(*attr.Name, "ecs.capability.")[1]
+						if strings.HasPrefix(capability, "docker-remote-api.") {
+							continue
+						}
+						if attr.Value == nil {
+							line = fmt.Sprintf(" - %s", capability)
+						} else {
+							line = fmt.Sprintf(" - %-22s %s", capability, colorstring.Color("[yellow]"+*attr.Value))
+						}
+						capabilities = append(capabilities, line)
+					} else {
+						if attr.Value == nil {
+						} else {
+							line = fmt.Sprintf(" - %s", *attr.Name)
+							line = fmt.Sprintf(" - %-22s %s", *attr.Name, colorstring.Color("[yellow]"+*attr.Value))
+						}
+						instanceAttributes = append(instanceAttributes, line)
+					}
+				}
+				fmt.Println("Attributes:")
+				sort.Strings(instanceAttributes)
+				for _, attr := range instanceAttributes {
+					fmt.Println(attr)
+				}
+				fmt.Println("Capabilities:")
+				sort.Strings(capabilities)
+				for _, attr := range capabilities {
+					fmt.Println(attr)
+				}
+				fmt.Println()
+			}
 		}
 		fmt.Println()
 	}
