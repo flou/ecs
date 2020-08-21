b093488cb41aca4c16a70bcbaf75ce26b1f25fb1
Lou Ferrand <ferrandlou@gmail.com>
2020-08-22 00:21:08 +0200

    Move version definition in main

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/flou/ecs/cmd"
 )
 
+var (
+	version string = "dev"
+	commit  string
+	date    string
+)
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(
+		buildVersion(version, commit, date),
+	)
+}
+
+func buildVersion(version, commit, date string) string {
+	var result = version
+	if commit != "" {
+		result = fmt.Sprintf("%s\ncommit: %s", result, commit)
+	}
+	if date != "" {
+		result = fmt.Sprintf("%s\nbuilt at: %s", result, date)
+	}
+	return result
 }
